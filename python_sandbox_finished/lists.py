bde4f0ef0b57028313cf246fd1bc14e7fd2e7790
Laura Quinn <lauraquinn@lauras-air.home>
2019-09-20 13:33:02 +0100

    Update list basics

--- python_sandbox_finished/lists.py
+++ python_sandbox_finished/lists.py
@@ -1,7 +1,7 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
 
 # Create list
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 6 , 7 , 8]
 fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
 
 # Use a constructor
@@ -38,3 +38,22 @@ fruits.sort()
 fruits.sort(reverse=True)
 
 print(fruits)
+
+# list slices, can also work on tuples
+print(fruits[1:3]) # Oranges, Mangos, includes first not last
+print(fruits[:3])  # Apples, Oranges, Mangos, from start until number
+print(fruits[2:])  # Mangos, Blueberries, from number until end
+print(numbers[::2]) # 1, 3, 5, 7 ; every other?
+print(numbers[1:8:2]) # 2, 4, 6 ,8 ; starting with index 1, until index 8, skip 2 index
+print(numbers[2:9:3]) # 3, 6 ; starting with index 2, until index 9, skip 3 index
+print(numbers[9:2:-3]) # 8, 5 ; starting with index 2, until index 9, skip 3 index
+# if last number is negative, happens backwards
+print(numbers[1:-1]) # 2, 3 ,4 ,5 ,6 ,7
+
+
+# list comprehension ; a list that obey a rule
+cubes = [i**3 for i in range(5)]
+print(cubes)
+# comprehension can also includes if statements
+evens=[i**2 for i in range(10) if i**2 % 2 == 0]
+print(evens)
