75a21bb3e27e61816e140df30fe26eafe9eb152f
Laura Quinn <lauraquinn@lauras-air.home>
2019-09-20 11:53:06 +0100

    Update dictionary basics

--- python_sandbox_finished/dictionaries.py
+++ python_sandbox_finished/dictionaries.py
@@ -1,5 +1,7 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 
+# similar to an object literal
+
 # Create dict
 person = {
     'first_name': 'John',
@@ -7,12 +9,27 @@ person = {
     'age': 30
 }
 
+# trying to access a key they doesn't exist ex: 'gender' returns KeyError
+
 # Use constructor
 # person2 = dict(first_name='Sara', last_name='Williams')
 
 # Get value
 print(person['first_name'])
 print(person.get('last_name'))
+
+# you can also you in/not in to check keys
+print('first_name' in person) #true
+print('gender' in person)     #false
+print('gender' not in person) #true
+
+# get method, similar to index but returns None if not found
+print(person.get('first_name')) # John
+print(person.get('email'))      # None
+print(person.get(12345, "not in dictionary")) # not in dictionary
+print(person.get('last_name', "not in dictionary")) # Doe
+
+
 
 # Add key/value
 person['phone'] = '555-555-5555'
