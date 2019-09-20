f2ee21f17ba3d5bb50c62d33f18bfe18a5297410
Laura Quinn <lauraquinn@lauras-air.home>
2019-09-20 13:53:48 +0100

    Create numeric functions file

---
+++ python_sandbox_finished/numeric.py
@@ -0,0 +1,8 @@
+# built in numeric functions
+
+print(min(1, 2, 3, 4, 0, 2, 1))   # find minimum number
+print(max([1, 4, 9, 2, 5, 6, 8])) # find max number
+print(abs(-99))                   # distance of a num from zero, absolute num
+print(abs(42))
+print(sum([1, 2, 3, 4, 5]))       # total sum of all numbers 
+# print(round()) # rounds number to a specified digit
