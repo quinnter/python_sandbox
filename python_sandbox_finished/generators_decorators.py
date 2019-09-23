70cbe53be9a29f6e7f3123ac4e48463bf7845f1b
Laura Quinn <lauraquinn@lauras-air.home>
2019-09-23 11:27:53 +0100

    Add more detail to how wrapper function works

--- python_sandbox_finished/generators_decorators.py
+++ python_sandbox_finished/generators_decorators.py
@@ -56,6 +56,7 @@ def decorator_function(original_function):
     def wrapper_function():
         print('wrapper executed this before {}'.format(original_function.__name__))
         return original_function()
+        print('wrapper executed this after {}'.format(original_function.__name__))
     return wrapper_function
 
  # decorator_function returns wrapper function which is waiting to be executed
